e798e3b71bb6e4da481f40bdc78ca344ea60b527
ahmedraufofficial <ahmedraufkh@outlook.com>
2024-01-04 16:45:05 +0400

    all

--- app.js
+++ app.js
@@ -29,8 +29,8 @@ import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
 // Routes live here; this is the C in MVC
-import routes from './routes/index.js';
-import { addServerSideRendering } from './server-side-rendering.js';
+import routes from './routes';
+import { addServerSideRendering } from './server-side-rendering';
 
 // Bootstrap Express and atlassian-connect-express
 const app = express();
